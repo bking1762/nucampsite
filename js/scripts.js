6656b1b46e11346aa2fa1f6ab797417b8b2766f8
Barbara King <bking1762@gmail.com>
2022-11-14 11:09:36 -0600

    wk 4

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,13 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
-    })
+    });
+});
 
-   // $("#loginButton").click(function(){
-   //     $("#loginModal").modal('show');
-  //  });
+   $("#loginButton").click(function(){
+       $("#loginModal").modal('show');
+   });
 
     $("#reserveButton").click(function(){
         $("#reserveModal").modal('show');
     });
-
-});
